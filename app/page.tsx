18421b1e3e946e2778b1bc7ffc2a374b1e7de497
Amumux <fabriali4@gmail.com>
2024-02-11 13:03:26 +0100

    Walmart-clone

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="">
-      <h1>Let's build walmart clone</h1>
+      <h1 className="">Let's build walmart clone</h1>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,20 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 import { Search } from "lucide-react";
 
+
 function Header() {
   return (
-    <header>
+    <header className="flex bg-walmart">
       <Link href="/">
         <Image
-          src="https://links.papareact.com/xsi"
+          src="https://links.papareact.com/yur"
           alt="Logo"
           width={150}
           height={150}
         ></Image>
       </Link>
 
-      <form className="flex items-center bg-white rounded-full w-full">
-        <input type="text" placeholder="Search Everything..."/>
+      <form className="flex items-center rounded-full w-full flex-1">
+        <input type="text" placeholder="Search Everything..." className="flex-1 px-4 rounded-full"/>
         <button>
           <Search 
             className="rounded-full h-10"
